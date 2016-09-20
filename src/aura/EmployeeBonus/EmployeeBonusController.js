bc18034e3635a6e52df13234f0c6b6153110eb07
Laura Meerkatz <lmeerkatz@salesforce.com>
2016-09-20 16:52:13 -0700

    Add data table to Lightning Component

--- src/aura/EmployeeBonus/EmployeeBonusController.js
+++ src/aura/EmployeeBonus/EmployeeBonusController.js
@@ -2,7 +2,7 @@
 	doInit : function(component, event, helper) {
 		var action = component.get("c.getEmployeeList");
         action.setCallback(this, function(response) {
-            component.set("v.employees", action.getReturnValue());
+            component.set("v.employees", response.getReturnValue());
         });
         $A.enqueueAction(action);
 	}
