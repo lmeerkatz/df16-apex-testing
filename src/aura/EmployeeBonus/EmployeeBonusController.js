cd07789fc6eeed8f94f7f9d5d7d12860d93890c6
Laura Meerkatz <lmeerkatz@salesforce.com>
2016-09-20 17:23:56 -0700

    Finish Lightning Component and related tests

--- src/aura/EmployeeBonus/EmployeeBonusController.js
+++ src/aura/EmployeeBonus/EmployeeBonusController.js
@@ -2,7 +2,20 @@
 	doInit : function(component, event, helper) {
 		var action = component.get("c.getEmployeeList");
         action.setCallback(this, function(response) {
-            component.set("v.employees", response.getReturnValue());
+            var state = response.getState();
+            if (state === "SUCCESS") {
+	            component.set("v.employees", response.getReturnValue());
+            } else if (state === "ERROR") {
+                var errors = response.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        console.log("Error message: " +
+                        errors[0].message);
+                    }
+                } else {
+                    console.log("Unknown error");
+                }
+            }
         });
         $A.enqueueAction(action);
 	}
